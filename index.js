517977570d4325cf500d36f30f4aa51d396c9262
Peter Jang <peter.x.jang@gmail.com>
2020-04-05 11:26:27 -0500

    Display array using v-for and add button to add new item to array

--- index.js
+++ index.js
@@ -3,11 +3,18 @@ var app = new Vue({
   data: {
     message: "Hello from JavaScript!",
     name: "Peter",
-    showExtraInfo: true,
+    showExtraInfo: false,
+    fruits: ["apple", "banana", "cantaloupe"],
+    newFruit: "",
   },
   methods: {
     changeMessage: function () {
       this.message = "Whoa...";
+    },
+    createFruit: function () {
+      console.log(this.newFruit);
+      this.fruits.push(this.newFruit);
+      this.newFruit = "";
     },
   },
 });
